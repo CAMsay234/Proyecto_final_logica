af03242371c833647a5eb5366fb423de188c8b31
Your Name <camilovelasquez2003@gmail.com>
2021-05-05 22:36:29 -0500

    interfaz gráfico modificado

--- Programa.java
+++ Programa.java
@@ -1,5 +1,13 @@
 public class Programa{
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+
+System.out.println("  _  __               _                        _        		");
+System.out.println(" | |/ /              | |                      | |       		");
+System.out.println(" | ' / __ _ _ __ ___ | | _____  __ _ _ __   __| | ___   		");
+System.out.println(" |  < / _` | '__/ _ \\| |/ / _ \\/ _` | '_ \\ / _` |/ _ \\  	");
+System.out.println(" | . \\ (_| | | | (_) |   <  __/ (_| | | | | (_| | (_) | 		");
+System.out.println(" |_|\\_\\__,_|_|  \\___/|_|\\_\\___|\\__,_|_| |_|\\__,_|\\___/  ");
+                                                        
+
 	}
 }
