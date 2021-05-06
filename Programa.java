e911e3451ad097493e76c195871b58eba0595080
Your Name <camilovelasquez2003@gmail.com>
2021-05-06 08:58:43 -0500

    acualización interfax gráfica

--- Programa.java
+++ Programa.java
@@ -8,6 +8,13 @@ System.out.println(" |  < / _` | '__/ _ \\| |/ / _ \\/ _` | '_ \\ / _` |/ _ \\  
 System.out.println(" | . \\ (_| | | | (_) |   <  __/ (_| | | | | (_| | (_) | 		");
 System.out.println(" |_|\\_\\__,_|_|  \\___/|_|\\_\\___|\\__,_|_| |_|\\__,_|\\___/  ");
                                                         
-
+System.out.println("|                                                       ");
+System.out.println("|                    Ingrese una opción así:            ");
+System.out.println("|                    1. Buscar canción                  ");
+System.out.println("|                    2. Reproducir Canción              ");
+System.out.println("|                    3. Mostrar Letra                   ");
+System.out.println("|                    4. Detener Canción                 ");
+System.out.println("|                    5. Imprimir lista de Canciones     ");
+System.out.println("|                    6. Salir                           ");
 	}
 }
